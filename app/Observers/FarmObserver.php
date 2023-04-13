c8c771ce424b9bd9f1921621404548748a4507b9
zoltalar <voytechp@gmail.com>
2023-04-13 09:26:18 +0200

    farm observer tweaks - composer update

--- app/Observers/FarmObserver.php
+++ app/Observers/FarmObserver.php
@@ -3,16 +3,11 @@
 namespace App\Observers;
 
 use App\Models\Farm;
-use Str;
 
 class FarmObserver
 {
     public function saving(Farm $farm)
     {
-        if ( ! empty($farm->website) && ! Str::startsWith($farm->website, 'http://')) {
-            $farm->website = 'http://' . $farm->website;
-        }
-
         if ($farm->active == 1) {
             $farm->deactivation_reason = null;
             $farm->deactivated_at = null;
