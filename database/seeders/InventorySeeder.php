d8ec755e3d81fae844791c43d5fad078fed9fb6e
zoltalar <voytechp@gmail.com>
2023-07-07 10:56:26 +0200

    black tea

--- database/seeders/InventorySeeder.php
+++ database/seeders/InventorySeeder.php
@@ -5956,6 +5956,32 @@ class InventorySeeder extends Seeder
                 'category' => 'Coffee and Teas',
                 'system' => 1,
                 'countries' => [],
+                'substances' => [
+                    [
+                        'name' => 'Calories',
+                        'value' => 1
+                    ],
+                    [
+                        'name' => 'Carbohydrates',
+                        'value' => 0.2,
+                        'value_unit' => Unit::ABBREVIATION_GRAM
+                    ],
+                    [
+                        'name' => 'Calcium',
+                        'value' => 1,
+                        'value_unit' => Unit::ABBREVIATION_MILIGRAM
+                    ],
+                    [
+                        'name' => 'Iron',
+                        'value' => 0.01,
+                        'value_unit' => Unit::ABBREVIATION_MILIGRAM
+                    ],
+                    [
+                        'name' => 'Potassium',
+                        'value' => 25,
+                        'value_unit' => Unit::ABBREVIATION_MILIGRAM
+                    ],
+                ],
                 'translations' => [
                     'Polish' => 'Czarna Herbata'
                 ]
